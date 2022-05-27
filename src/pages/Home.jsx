57143fb711b0be51ca66c1364f4d05c49e720e40
datamaster04 <datamaster04@gmail.com>
2022-05-27 09:02:33 +0200

    image fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "../style/App.css";
 import "../style/Home.css";
+import background from "../assets/images/LogoTreningssenterVinger.png";
 
 const Home = (props) => {
-    return (
-        <section>
-            
-            <div className="">
-
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        Hello World
+      </div>
+    </section>
+  );
 };
 
 export default Home;
