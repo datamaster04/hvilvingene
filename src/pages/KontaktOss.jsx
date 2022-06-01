18153c498da6704f3af96c732fb61abc8b9ed2a1
datamaster04 <datamaster04@gmail.com>
2022-06-01 11:19:42 +0200

    first example done

--- src/pages/KontaktOss.jsx
+++ src/pages/KontaktOss.jsx
@@ -2,10 +2,108 @@ import React from "react";
 import "../style/App.css";
 import "../style/KontaktOss.css";
 
+const åpningstider = [
+  {
+    day: "Mandag",
+    tider: "06.30-23.00",
+  },
+  {
+    day: "Tirsdag",
+    tider: "06.30-23.00",
+  },
+  {
+    day: "Onsdag",
+    tider: "07.00-23.00",
+  },
+  {
+    day: "Torsdag",
+    tider: "07.00-23.00",
+  },
+  {
+    day: "Fredag",
+    tider: "08.00-23.00",
+  },
+  {
+    day: "Lørdag",
+    tider: "08.30-22.30",
+  },
+  {
+    day: "Søndag",
+    tider: "09.00-22.30",
+  },
+];
+
+const informasjon = [
+    {
+        informasjon: "Adresse",
+        id: "Brygga 20, 2317 Hamar",
+    },
+
+    {
+        informasjon: "Telefonnummer",
+        id: "62 53 44 45",
+    },
+
+    {
+        informasjon: "Mail",
+        id: "datamaster04@gmail.com"
+    }
+];
+
 const KontaktOss = (props) => {
   return (
     <section>
-      <div className=""></div>
+      <div className="KontaktOss-section">
+        <div className="KontaktOss-header">Kontakt Oss</div>
+        <div className="container-KontaktOss">
+          <div className="form-section-KontaktOss">
+            <div className="form-KontaktOss">
+              <iframe
+                className="form-KontaktOss"
+                src="https://forms.gle/MmBuiEkWrWC1yKrP8"
+                width="1000px"
+                height="1009px"
+                alt="google form custom"
+              ></iframe>
+            </div>
+          </div>
+
+          <div className="KontaktOss-opening-section">
+            <div className="KontaktOss-opening-header">Åpningstider</div>
+            <div className="KontaktOss-opening">
+              {åpningstider.map((tid, idx) => (
+                <p className="dager" key={idx}>
+                  {tid.day}: {tid.tider}
+                </p>
+              ))}
+            </div>
+          </div>
+          
+        </div>
+        <div className="kontaktinformasjon-section">
+        <div className="maps-section">
+            <iframe 
+            className="maps"
+            src="https://maps.google.com/maps?q=Brygga%2020,%202317%20Hamar
+            &t=k&z=13&ie=UTF8&iwloc=&output=embed"
+            width="1000px"
+            height="500px"
+            alt="google maps - actic"
+            ></iframe>
+        </div>
+        <div className="kontaktinformasjon"> 
+            <div className="maps-Kontaktoss"></div>
+            <div className="kontaktinformasjon-header">Kontaktinformasjon</div>
+            <div className="infromasjon-KontaktOss">
+              {informasjon.map((info, idx) => (
+                <p className="informasjon" key={idx}>
+                  {info.informasjon}: {info.id}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/pages/OmOss.jsx
+++ src/pages/OmOss.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import "../style/App.css";
 import "../style/OmOss.css";
+import BildeOmOss from "../assets/images/BildeOmOss.png";
 
 const OmOss = (props) => {
   return (
     <section>
-      <div className=""></div>
+      <div className="OmOss-section">
+        <div className="OmOss-bilde">
+          <img class="BildeOmOss" src={BildeOmOss} />
+        </div>
+        <div className="texts">
+          <div className="header-OmOss">
+            Om Oss
+            <div className="text-OmOss">
+              Vi er en gruppe trenings entusiaster, som startet opp nylig i
+              2021. Vi tilbyr trenings fasiliteter i høye kvaliteter, sentralt
+              beliggenhet i Hamar, og en høy standard. Kom innom på en kopp
+              kaffe og titt på senteret vårt!
+            </div>
+          </div>
+          <div className="header-fasiliteter">
+            Fasiliteter
+            <div className="text-fasiliteter">
+              Blant annet store garderober og badstue, store relaxområder og
+              barnepark med mulighet for barnepass, og mye mer!
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
